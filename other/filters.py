c7050055f73966dc684a17c650b35dab12dfb37a
danikkkkk12 <danilmalyk20@gmail.com>
2025-03-16 14:57:09 +0200

    +

--- other/filters.py
+++ other/filters.py
@@ -1,24 +1,37 @@
 from typing import Any
 from aiogram.filters import BaseFilter
 from aiogram import types, Bot
+from aiogram.types import ChatMemberStatus  # Импортируем ChatMemberStatus
 
 from config import CHANNEL_ID
 from database.db import DataBase
 
 
 class ChatJoinFilter(BaseFilter):
-
     async def __call__(self, message: types.Message, bot: Bot) -> Any:
-        chat_member = await bot.get_chat_member(chat_id=CHANNEL_ID,
-                                                user_id=message.from_user.id)
+        try:
+            # Получаем информацию о пользователе в канале
+            chat_member = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=message.from_user.id)
 
-        if chat_member.status.value in ["member", "creator", "administrator"]:
-            return True
+            # Проверяем статус пользователя
+            if chat_member.status in [ChatMemberStatus.MEMBER, ChatMemberStatus.CREATOR, ChatMemberStatus.ADMINISTRATOR]:
+                return True
+
+        except Exception as e:
+            # Логируем ошибку, если что-то пошло не так
+            print(f"Ошибка при проверке участника канала: {e}")
 
         return False
 
 
 class RegisteredFilter(BaseFilter):
+    async def __call__(self, message: types.Message) -> Any:
+        try:
+            # Проверяем, зарегистрирован ли пользователь в базе данных
+            user = await DataBase.get_user(message.from_user.id)
+            return user is not None
 
-    async def __call__(self, callback: types.CallbackQuery) -> Any:
-        return not await DataBase.get_user(callback.from_user.id) is None
+        except Exception as e:
+            # Логируем ошибку, если что-то пошло не так
+            print(f"Ошибка при проверке регистрации пользователя: {e}")
+            return False
