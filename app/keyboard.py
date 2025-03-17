92022826daf297eefe7c4114f6c0dc8460308b41
danikkkkk12 <danilmalyk20@gmail.com>
2025-03-17 14:33:56 +0200

    +

--- app/keyboard.py
+++ app/keyboard.py
@@ -1,11 +1,19 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from config import *
 
+# Клавиатура для регистрации
 reg = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='📝 Регистрация', url=REF)],
     [InlineKeyboardButton(text='✅ Готово', callback_data='yes')]
 ])
 
+# Клавиатура для депозита после регистрации
+deposit = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='💸 Депозит', url=DEPOSIT_LINK)],
+    [InlineKeyboardButton(text='🆘 Помощь', url=SUPPORT_LINK)]
+])
+
+# Главное меню после депозита
 regget = InlineKeyboardMarkup(inline_keyboard=[ 
     [InlineKeyboardButton(text='💣 Получить сигнал', callback_data='play')],
     [InlineKeyboardButton(text='📌 О боте', callback_data='info'), InlineKeyboardButton(text='🎙 Канал', url=CHANNEL)]
@@ -17,6 +25,7 @@ owner = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='💻 Панель администратора', callback_data='admin_panel')]
 ])
 
+# Игры
 games = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='🚀 LuckyJet', web_app=WebAppInfo(url='https://volneer.github.io/jea/')), InlineKeyboardButton(text='✈️ Aviator', web_app=WebAppInfo(url='https://volneer.github.io/avai/'))],
     [InlineKeyboardButton(text='💣 Mines', web_app=WebAppInfo(url='https://onezelenka.github.io/Blume7Games/mines/')), InlineKeyboardButton(text='👑 RoyalMines', web_app=WebAppInfo(url='https://volneer.github.io/raayl/'))],
@@ -24,15 +33,18 @@ games = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='⬅️ Назад', callback_data='back')]
 ])
 
+# Сохранение настроек
 save = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='✅ Сохранить', callback_data='save'), InlineKeyboardButton(text='🔁 Изменить', callback_data='no_save')]
 ]) 
 
+# Панель администратора
 admin_panel = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='🎙 Рассылка', callback_data='spam')],
     [InlineKeyboardButton(text='📊 Статистика', callback_data='stat')]
 ])
 
+# Статистика
 stat = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='⬅️ Назад', callback_data='back_admin')]
 ])
