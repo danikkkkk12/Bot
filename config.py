84fe3a191e3c323c2ecd34d69cfb65cfcadb484c
danikkkkk12 <danilmalyk20@gmail.com>
2025-03-17 10:03:04 +0200

    +

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = '7906839757:AAFN4ll3FATz9pl1LVxZJKO-GdxLDX0GXyc'
-REF = ''
+REF = 'https://1whhzm.top/?open=register&p=3eei'
 CHANNEL = 'https://t.me/CrazyMines777'
 NAME = 'CrazyMines'
 ADMIN_ID = 832597017
